c2c52b6f741cf392eda8cd1cb1b9f0ae2d02b2dd
Daniel Schneider <daschne8@gmail.com>
2019-03-04 20:34:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,65 +1,29 @@
 require 'pry'
 
-# Change these methods so that instead of using a name, we just have a number -- which starts at 267
-# No method takes any arguments
-$katz_deli = [265,266]
-$number = 267
-def line
+def line(places)
   line_string = ""
-  if $katz_deli.length == 0
+  if places.length == 0
     puts "The line is currently empty."
   else
     line_string = "The line is currently:"
-    $katz_deli.each_with_index do |name, index|
+    places.each_with_index do |name, index|
         line_string += " #{index+1}. #{name}"
     end
     puts line_string
   end
 end
 
-def take_a_number
-  $number += 1
-  $katz_deli.push($number)
-  num = $katz_deli.length
-  puts "Welcome, #{$number}. You are number #{num} in line."
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  num = katz_deli.length
+  puts "Welcome, #{name}. You are number #{num} in line."
 end
 
-def now_serving
-  if $katz_deli.length == 0
+def now_serving(katz_deli)
+  if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   else
-    serving = $katz_deli.shift
+    serving = katz_deli.shift
     puts "Currently serving #{serving}."
   end
 end
-
-binding.pry
-puts "goodbye from pry"
-
-# def line(places)
-#   line_string = ""
-#   if places.length == 0
-#     puts "The line is currently empty."
-#   else
-#     line_string = "The line is currently:"
-#     places.each_with_index do |name, index|
-#         line_string += " #{index+1}. #{name}"
-#     end
-#     puts line_string
-#   end
-# end
-#
-# def take_a_number(katz_deli, name)
-#   katz_deli.push(name)
-#   num = katz_deli.length
-#   puts "Welcome, #{name}. You are number #{num} in line."
-# end
-#
-# def now_serving(katz_deli)
-#   if katz_deli.length == 0
-#     puts "There is nobody waiting to be served!"
-#   else
-#     serving = katz_deli.shift
-#     puts "Currently serving #{serving}."
-#   end
-# end
